592adae870576054e08509cacad828457db9c5fe
gusev-iliya <guseviliya111@gmail.com>
2021-10-19 17:13:15 +0300

    1

--- cam.py
+++ cam.py
@@ -17,16 +17,31 @@ def index():
 
     novaya = list(cursor.execute(f'SELECT * FROM Novaya').fetchone())
     class Novaya():
-        def __init__(self, novaya):
-            self.data = eval(str(novaya[6]))
+        def __init__(self, cursor):
+            self.a = list(cursor.execute(f'SELECT * FROM Novaya').fetchone())
+            self.all = list(cursor.execute(f'SELECT * FROM Novaya').fetchall())
+            self.data = eval(str(self.a[6]))
             self.names = ','.join(list(self.data.keys()))
+            self.names1= list(self.data.keys())
             self.len = len(list(self.data.keys()))
+            self.data1 = {}
+            self.labels=[]
+            print()
+            for i in list(self.data.keys()):
+                self.data1[i] = []
+            for j in self.all:
+                self.labels.append(j[3])
+                for k in list(self.data.keys()):
+                    self.data1[k].append(eval(str(j[6]))[k])
 
-    novaya = Novaya(novaya)
+
+
+    novaya = Novaya(cursor)
 
     return render_template('main_tem.html',points=points,file=f,novaya=novaya)
-
-
+@app.route('/t')
+def index1():
+    return render_template('t.html',)
 
 if __name__ == '__main__':
     app.run(debug = True)
